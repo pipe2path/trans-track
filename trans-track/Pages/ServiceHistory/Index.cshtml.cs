0ffe888a76b732681b51373d448d87a72e63457e
kevinmenezes <kevinmenezes@yahoo.com>
2019-06-20 12:22:59 -0700

    title changes to Vehicle pages

--- trans-track/Pages/ServiceHistory/Index.cshtml.cs
+++ trans-track/Pages/ServiceHistory/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using trans_track.Models;
 
 namespace trans_track.Pages.ServiceHistory
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly trans_track.Data.ApplicationDbContext _context;
